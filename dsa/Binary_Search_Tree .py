47512ecd05ff2e2273de6db48058eba9cc491381
Arun Baruah <arun.baruah@outlook.com>
2026-09-11 16:52:44 +0530

    dsa binary tree updated

---
+++ dsa/Binary_Search_Tree .py
@@ -0,0 +1,36 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if root.val < key:
+            root.right = insert(root.right, key)
+        else:
+            root.left = insert(root.left, key)
+    return root
+
+if __name__ == '__main__':
+    # Create a BST and insert some keys
+    r = Node(50)
+    r = insert(r, 30)
+    r = insert(r, 20)
+    r = insert(r, 40)
+    r = insert(r, 70)
+    r = insert(r, 60)
+    r = insert(r, 80)
+
+    # Print inorder traversal of the BST to produce output
+    def inorder(root):
+        if root:
+            inorder(root.left)
+            print(root.val, end=" ")
+            inorder(root.right)
+
+    print("Inorder traversal of the given tree:")
+    inorder(r)
+    print()
